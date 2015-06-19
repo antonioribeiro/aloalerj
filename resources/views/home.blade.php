39f9a66bcf6e4f93bbb6367879869d3c9a5bf0ee
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-19 15:34:04 -0300

    Alo alerj

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,153 +1,145 @@
 @extends('layout')
 
 @section('content')
-    <div id="application">
-        <nav class="fixed-nav-bar">
-            <img id="topbar" class="img-top-bar" src="assets/img/topbar.jpg" alt=""/>
-        </nav>
+    <div class="link" id="home-logo" v-on="click: onClick"></div>
 
-        <div class="link" id="home-logo" v-on="click: onClick"></div>
-
-        <a href="{!! url('find') !!}" class="link" id="home-loupe"></a>
-        <a class="link" id="home-menu"></a>
-        <a transition="slide" class="link" id="home-alo" href="{!! url('find') !!}"></a>
-
-        <a class="link" id="home-phone"></a>
-        <a class="link" id="home-chat"></a>
-        <a class="link" id="home-audio"></a>
-        <a class="link" id="home-video"></a>
-        <a class="link" id="home-photo"></a>
-        <a class="link" id="home-file"></a>
-        <a class="link" id="home-messages"></a>
-        <a class="link" id="home-news"></a>
-        <a class="link" id="home-ordem"></a>
-        <a class="link" id="home-tv"></a>
-        <a class="link" id="home-laws"></a>
-        <a class="link" id="home-alerj"></a>
-        <a class="link" id="home-transparency"></a>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <a transition="slide" href="simple2.html">Flip to page two</a>
-    </div>
+    <a class="link" id="home-loupe" href="{!! url('find') !!}"></a>
+    <a class="link" id="home-menu"></a>
+    <a class="link" id="home-alo" href="{!! url('find') !!}"></a>
+    <a class="link" id="home-phone" href="{!! url('aloalerj') !!}"></a>
+    <a class="link" id="home-chat"></a>
+    <a class="link" id="home-audio"></a>
+    <a class="link" id="home-video"></a>
+    <a class="link" id="home-photo"></a>
+    <a class="link" id="home-file"></a>
+    <a class="link" id="home-messages"></a>
+    <a class="link" id="home-news"></a>
+    <a class="link" id="home-ordem"></a>
+    <a class="link" id="home-tv"></a>
+    <a class="link" id="home-laws"></a>
+    <a class="link" id="home-alerj"></a>
+    <a class="link" id="home-transparency"></a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>ALERJ</title>
 
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -11,13 +11,19 @@
     </head>
 
     <body>
-        @yield('content')
+        <div id="application">
+            <nav class="fixed-nav-bar">
+                <img id="topbar" class="img-top-bar" src="assets/img/topbar.jpg" alt=""/>
+            </nav>
+
+            @yield('content')
+        </div>
 
         <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script>jQuery.noConflict();</script>
 
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js"></script>
-        <script type="text/javascript" src="assets/packages/zepto-page-transitions/min/transition.min.js"></script>
+        {{--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js"></script>--}}
+        {{--<script type="text/javascript" src="assets/packages/zepto-page-transitions/min/transition.min.js"></script>--}}
         <script src="assets/js/app.js"></script>
     </body>
 </html>
