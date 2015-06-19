f3379f8dae27e0eae4a5e26720768bec7987b2fe
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-19 17:54:48 -0300

    Update Map

--- resources/views/pages/aloalerj.blade.php
+++ resources/views/pages/aloalerj.blade.php
@@ -12,6 +12,7 @@
             <area shape="rect" coords="3,3,638,104" href="/disque/aloalerj">
             <area shape="rect" coords="4,108,637,209" href="/disque/crianca">
             <area shape="rect" coords="5,1436,637,1540" href="/disque/mulher">
+            <area shape="rect" coords="3,1204,638,1316" href="/disque/ambiental">
         </map>
     </div>
 @stop
