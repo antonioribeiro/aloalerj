87272398f893be228980c298cc58e4daf301b36e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-19 18:18:11 -0300

    Update Map

--- resources/views/pages/aloalerj.blade.php
+++ resources/views/pages/aloalerj.blade.php
@@ -9,10 +9,10 @@
         <img class="main" src="/assets/img/aloalerj.jpg" usemap="#links"/>
 
         <map name="links">
-            <area shape="rect" coords="0,180,640,298" href="/disque/aloalerj">
-            <area shape="rect" coords="0,298,640,416" href="/disque/crianca">
-            <area shape="rect" coords="0,416,640,534" href="/disque/mulher">
-            <area shape="rect" coords="0,534,640,652" href="/disque/ambiental">
+            <area shape="rect" coords="0,  0,640,180" href="/disque/aloalerj">
+            <area shape="rect" coords="0,180,640,298" href="/disque/crianca">
+            <area shape="rect" coords="0,298,640,416" href="/disque/mulher">
+            <area shape="rect" coords="0,416,640,534" href="/disque/ambiental">
         </map>
     </div>
 @stop
