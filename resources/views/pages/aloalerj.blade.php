68f15e52ed864b9e83f0727f5a898e04c49960f3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-19 17:37:02 -0300

    Map

--- resources/views/pages/aloalerj.blade.php
+++ resources/views/pages/aloalerj.blade.php
@@ -1,9 +1,17 @@
 @extends('layout')
 
 @section('content')
-    <div class="spinner">
-        <i class="fa fa-spinner fa-pulse"></i>
-    </div>
+    {{--<div class="spinner">--}}
+        {{--<i class="fa fa-spinner fa-pulse"></i>--}}
+    {{--</div>--}}
 
-    <img class="main" src="/assets/img/aloalerj.jpg"/>
+    <div id="imagemap">
+        <img class="main" src="/assets/img/aloalerj.jpg" usemap="links"/>
+
+        <map name="links">
+            <area shape="rect" coords="110,110,82,126" href="sun.htm" alt="Sun">
+            <area shape="circle" coords="90,58,3" href="mercur.htm" alt="Mercury">
+            <area shape="circle" coords="124,58,8" href="venus.htm" alt="Venus">
+        </map>
+    </div>
 @stop
