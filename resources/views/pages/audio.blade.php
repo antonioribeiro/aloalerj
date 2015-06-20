831251490e4f15fad80cd5195ad14f96c2a79ba1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-20 12:39:30 -0300

    header

---
+++ resources/views/pages/audio.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+    <div class="spinner">
+        <i class="fa fa-spinner fa-pulse"></i>
+    </div>
+
+    <div class="background">
+        <img class="main" src="/assets/img/audio/GravacaoAudio_01.jpg" />
+    </div>
+
+    <a class="hiddenLink" style="top: 1050px; left: 380px; height: 230px; width: 220px;" href="{!! url('chat/message') !!}"></a>
+@stop
