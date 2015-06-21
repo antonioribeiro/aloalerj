54b2558ea15fc887d27bbaafae47df57d4a63d2b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-06-21 19:03:51 -0300

    Add more icons

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,10 +8,18 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="/assets/css/main.css"/>
 
-        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/img/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/img/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/img/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" href="/assets/img/icons/icon/ios/Icon-72@2x.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="50x50" href="/assets/img/icons/icon/ios/Icon-Small-50.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/img/icons/icon/ios/Icon.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/img/icons/icon/ios/Icon-60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/img/icons/icon/ios/Icon-72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="100x100" href="/assets/img/icons/icon/ios/Icon-Small-50@2x.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/img/icons/icon/ios/Icon@2x.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/icons/icon/ios/Icon-72@2x.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="512x512" href="/assets/img/icons/icon/ios/iTunesArtwork.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="1024x1024" href="/assets/img/icons/icon/ios/iTunesArtwork@2x.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/icon/ios/Icon-32.png">
 
         <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/0.12.1/vue.min.js"></script>
     </head>
